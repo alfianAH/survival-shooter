e1ab60e6cd5771171ada8ac4ddc15b94c4f9c0a5
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-01 23:24:59 +0800

    Spawning enemy

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -7,21 +7,20 @@ public class EnemyManager : MonoBehaviour
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
-
-    void Start ()
+    private void Start ()
     {
+        // Invoke Spawn every spawnTime
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
-
-    void Spawn ()
+    private void Spawn ()
     {
-        if (playerHealth.currentHealth <= 0f)
-        {
-            return;
-        }
-
+        // If player is dead, then don't make new enemy
+        if (playerHealth.currentHealth <= 0f) return;
+        
+        // Get random number
         int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        // Duplicate enemy
         Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 
     }
