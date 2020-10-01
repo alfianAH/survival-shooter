74d5ead7ed09c2473c3c9c97a3bb4d521571578a
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-01 17:41:18 +0800

    Make camera follow player

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        // Get offset between target and camera
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        // Get camera position
+        Vector3 targetCamPos = target.position + offset;
+        
+        // Set camera position
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
