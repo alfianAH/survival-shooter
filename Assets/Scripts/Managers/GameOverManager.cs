7ee6ccb392142575ba388ecd1a7ab87209d36833
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-02 17:58:52 +0800

    Make warning system

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -1,23 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;       
-    public float restartDelay = 5f;            
-
-
-    Animator anim;                          
-    float restartTimer;                    
-
-
-    void Awake()
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+    public Text warningText;
+    private Animator anim;
+    private float restartTimer;
+    
+    private void Awake()
     {
         anim = GetComponent<Animator>();
     }
-
-
-    void Update()
+    
+    private void Update()
     {
         if (playerHealth.currentHealth <= 0)
         {
@@ -30,5 +28,11 @@ public class GameOverManager : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = $"! {Mathf.RoundToInt(enemyDistance)} m";
+        anim.SetTrigger("Warning");
     }
 }
---
+++ Assets/Scripts/Player/PlayerDetection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public GameOverManager gameOverManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && !other.isTrigger)
+        {
+            float enemyDistance = Vector3.Distance(transform.position, other.transform.position);
+            gameOverManager.ShowWarning(enemyDistance);
+        }
+    }
+}
