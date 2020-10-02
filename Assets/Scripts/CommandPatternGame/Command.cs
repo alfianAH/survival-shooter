23df9a248f099fd53fdd43374eb8e5b024478902
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-02 16:40:39 +0800

    Implement command pattern in game

---
+++ Assets/Scripts/CommandPatternGame/Command.cs
@@ -0,0 +1,50 @@
+﻿namespace CommandPatternGame
+{
+    public abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+
+    public class MoveCommand : Command
+    {
+        private PlayerMovement playerMovement;
+        private float horizontal, vertical;
+    
+        public MoveCommand(PlayerMovement playerMovement, float horizontal, float vertical)
+        {
+            this.playerMovement = playerMovement;
+            this.horizontal = horizontal;
+            this.vertical = vertical;
+        }
+    
+        public override void Execute()
+        {
+            playerMovement.Move(horizontal, vertical);
+            playerMovement.WalkingAnimation(horizontal, vertical);
+        }
+
+        public override void UnExecute()
+        {
+            playerMovement.Move(-horizontal, -vertical);
+            playerMovement.WalkingAnimation(horizontal, vertical);
+        }
+    }
+
+    public class ShootingCommand : Command
+    {
+        private PlayerShooting playerShooting;
+
+        public ShootingCommand(PlayerShooting playerShooting)
+        {
+            this.playerShooting = playerShooting;
+        }
+    
+        public override void Execute()
+        {
+            playerShooting.Shoot();
+        }
+
+        public override void UnExecute() { }
+    }
+}
---
+++ Assets/Scripts/CommandPatternGame/InputHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommandPatternGame
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public PlayerMovement playerMovement;
+        public PlayerShooting playerShooting;
+        
+        private Queue<Command> commands = new Queue<Command>();
+        
+        private void FixedUpdate()
+        {
+            Command moveCommand = InputMovementHandling();
+            if (moveCommand != null)
+            {
+                commands.Enqueue(moveCommand);
+                moveCommand.Execute();
+            }
+        }
+
+        private void Update()
+        {
+            Command shootCommand = InputShootHandling();
+            if (shootCommand != null)
+            {
+                shootCommand.Execute();
+            }
+        }
+
+        private Command InputMovementHandling()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                return new MoveCommand(playerMovement, 0, 1);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                return new MoveCommand(playerMovement, -1, 0);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                return new MoveCommand(playerMovement, 0, -1);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                return new MoveCommand(playerMovement, 1, 0);
+            }
+
+            if (Input.GetKey(KeyCode.Z))
+            {
+                return Undo();
+            }
+
+            return new MoveCommand(playerMovement, 0, 0);
+        }
+
+        private Command Undo()
+        {
+            // If command Queue is not null, undo
+            if (commands.Count > 0)
+            {
+                Command undoCommand = commands.Dequeue();
+                undoCommand.UnExecute();
+            }
+
+            return null;
+        }
+
+        private Command InputShootHandling()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                return new ShootingCommand(playerShooting);
+            }
+
+            return null;
+        }
+    }
+}
