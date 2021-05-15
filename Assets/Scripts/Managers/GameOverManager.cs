0186e214cefd1b00e4bf03c8dcb87e4aa724abf8
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-05-16 00:05:57 +0800

    Add game over animation

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    private PlayerHealth playerHealth; // Reference to the player's health.
+    private Animator anim; // Reference to the animator component.
+    private bool stopPlay;
+    private static readonly int GameOver = Animator.StringToHash("GameOver");
+    
+    private void Awake ()
+    {
+        // Set up the reference.
+        anim = GetComponent <Animator> ();
+        playerHealth = PlayerHealth.Instance;
+    }
+    
+    private void Update ()
+    {
+        // If the player has run out of health...
+        if(playerHealth.currentHealth <= 0 && !stopPlay)
+        {
+            // ... tell the animator the game is over.
+            stopPlay = true;
+            anim.SetTrigger (GameOver);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -37,7 +37,6 @@ public class PlayerHealth : MonoBehaviour
     public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-    public UnityEvent onGameOver;
     
     private Animator anim;
     private AudioSource playerAudio;
@@ -140,7 +139,5 @@ public class PlayerHealth : MonoBehaviour
         // Disable PlayerMovement script
         playerMovement.enabled = false;
         playerShooting.enabled = false;
-
-        onGameOver?.Invoke();
     }
 }
